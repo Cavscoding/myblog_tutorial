e5208b8f903b9362cc71888dd0769cb6fa37f8bb
Fast <fast@tangdeMacBook-Pro.local>
2016-07-03 21:22:50 +0800

    comment, markdown, category, search, readmore, rss and paginator

--- article/models.py
+++ article/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Article(models.Model):
 	title = models.CharField(max_length = 100) #博客标题
@@ -9,8 +10,13 @@ class Article(models.Model):
 	def __unicode__(self):
 		return self.title
 
+	#获取URL并转换成url的表示格式
+	def get_absolute_url(self):
+		path = reverse('detail', kwargs={'id': self.id})
+		return "http://127.0.0.1:8000%s" % path
+	
 	#def __str__(self):
-	#	return self.title.encode('utf-8')
+	#	return self.title
 
 	class Meta:  #按时间下降排序
 		ordering = ['-date_time']
--- article/views.py
+++ article/views.py
@@ -3,17 +3,76 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
+from django.http import Http404
+from django.contrib.syndication.views import Feed
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
-	post_list = Article.objects.all()   #获取全部Article对象
+	posts = Article.objects.all()   #获取全部Article对象
+	paginator = Paginator(posts, 4)     #每页显示五篇文章
+	page = request.GET.get('page')
+	try:
+		post_list = paginator.page(page)
+	except PageNotAnInteger:
+		post_list = paginator.page(1)
+	except EmptyPage:
+		post_list = paginator.paginator(paginator.num_pages)
 	return render(request, 'home.html', {'post_list': post_list})
 
-#def detail(request, my_args):
-#	post = Article.objects.all()[int(my_args)]
-#	str = ('title = %s, category = %s, date_time = %s, content = %s'
-#		% (post.title, post.category, post.date_time, post.content))
-#	return HttpResponse(str)
+def detail(request, id):
+	try:
+		post = Article.objects.get(id=str(id))
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'post.html', {'post': post})
 
-#def test(request):
-#	return render(request, 'test.html', {'current_time': datetime.now()})
+def archives(request):
+	try:
+		post_list = Article.objects.all()
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'archives.html', {'post_list': post_list, 'error': False})
+
+def about_me(request):
+	return render(request, 'aboutme.html')
+
+def search_tag(request, tag):
+	try:
+		post_list = Article.objects.filter(category__iexact = tag) #contains
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'tag.html', {'post_list': post_list})
+
+def blog_search(request):
+	if 's' in request.GET:
+		s = request.GET['s']
+		if not s:
+			return render(request, 'home.html')
+		else:
+			post_list = Article.objects.filter(title__icontains = s)
+			if len(post_list) == 0:
+				return render(request, 'archives.html', {'post_list': post_list, 'error': True})
+			else:
+				return render(request, 'archives.html', {'post_list': post_list, 'error': False})
+	return redirect('/')
+	
+
+class RSSFeed(Feed):
+	title = "RSS feed - article"
+	link = "feeds/posts/"
+	description = "RSS feed - blog posts"
+
+	def items(self):
+		return Article.objects.order_by('-date_time')
+
+	def item_title(self, item):
+		return item.title
+
+	def item_pubdate(self, item):
+		return item.date_time
+
+	def item_description(self, item):
+		return item.content
+
+
 
